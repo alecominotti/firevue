d254cd56fb7ca0389dc80184bfc2d2234000beb7
Ale <alecominotti@gmail.com>
2021-08-25 20:29:05 -0300

    Primer commit

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login-container">
+    <button @click="loginSubmit">Login with Google</button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  methods: {
+    loginSubmit(){
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+      //firebase.auth().signInWithRedirect(provider)
+        .catch(error => { console.log(error) })
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  .login-container {
+    width: 100%;
+    text-align: center;
+    padding-top: 50px;
+    button {
+      width: 50%;
+      background-color: #4e37fc;
+      color: white;
+      font-weight: bold;
+      font-size: 18pt;
+      padding: 20px;
+      border-radius: 15px;
+      border: none;
+      box-shadow: #00000040 0px 5px 10px;
+      cursor: pointer;
+      &:hover {
+        background-color: #7562ff;
+      }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
-new Vue({
+const firebaseConfig = {
+  apiKey: "AIzaSyAURYNootuClP2BvR-TcGnmBzU5M2QyGd4",
+  authDomain: "firevue-group-chat.firebaseapp.com",
+  projectId: "firevue-group-chat",
+  storageBucket: "firevue-group-chat.appspot.com",
+  messagingSenderId: "1059243269702",
+  appId: "1:1059243269702:web:7fc008d00dfadc09a6b447",
+  measurementId: "G-2HCJQSK4PQ"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+Vue.use(BootstrapVue)
+
+firebase.auth().onAuthStateChanged(()=> new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app'))
